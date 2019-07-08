ffe7361a971ab978dfaaeb693272a09dbd361d98
Matt Morris <matt.m.morris0@gmail.com>
2019-07-07 18:45:44 -0700

    Renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'firebase-project'`, () => {
+  it(`should have as title 'realtime-firebase-game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firebase-project');
+    expect(app.title).toEqual('realtime-firebase-game');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to firebase-project!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to realtime-firebase-game!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'firebase-project';
+  title = 'realtime-firebase-game';
 
   constructor() {
 
